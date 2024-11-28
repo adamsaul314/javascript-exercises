ad355cfd8e98f2d93b972016557db520c53dec5b
Adam Saul <adamsaul314@gmail.com>
2024-11-28 00:30:38 +0000

    completed repeat strings

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,27 @@
-const repeatString = function() {
 
+const repeatString = function(word, num) {
+    
+    let string = '';
+    let error = 'ERROR'
+
+    if(num < 0)
+    {
+        return error;
+    }
+    else
+    {
+
+        for (i = 0; i < num; i++) {
+        
+            string += word;
+    
+        }
+    
+        return string
+
+    }
+
+    
 };
 
 // Do not edit below this line
